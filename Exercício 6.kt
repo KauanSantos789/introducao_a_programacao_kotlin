854cbdd1079405af8ba9c26528562f9045e468b3
Kauan Santos <125403442+KauanSantos789@users.noreply.github.com>
2024-02-28 22:23:05 -0300

    Add files via upload

---
+++ Exercício 6.kt
@@ -0,0 +1,17 @@
+/* Exercício 6. Corrija o erro neste programa, para que ele produza a saída desejada.
+
+fun main() {
+    println("Cloudy") println("Partly Cloudy") println("Windy")
+}
+
+Saída desejada:
+
+Cloudy
+Partly Cloudy
+Windy
+
+Resposta: */ fun main() {
+                 println("Cloudy") 
+                 println("Partly Cloudy") 
+                 println("Windy")
+             }
