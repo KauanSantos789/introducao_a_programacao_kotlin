487b096ab822255ee83f551b2e06e3691932479b
Kauan Santos <125403442+KauanSantos789@users.noreply.github.com>
2024-02-28 22:23:27 -0300

    Add files via upload

---
+++ Exercício 7.kt
@@ -0,0 +1,13 @@
+/* Exercício 7. Corrija o erro neste programa, para que ele produza a saída desejada.
+
+fun main() (
+    println("How's the weather today?")
+)
+
+Saída desejada:
+
+How's the weather today?
+
+Resposta: */ fun main() {
+    	         println("How's the weather today?")
+             }
