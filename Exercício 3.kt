e756143b89d7327ad59a4f3183a0e1b9d0d4f327
Kauan Santos <125403442+KauanSantos789@users.noreply.github.com>
2024-02-28 22:22:06 -0300

    Add files via upload

---
+++ Exercício 3.kt
@@ -0,0 +1,25 @@
+/* Exercício 3. Copie e cole este programa no Kotlin Playground.
+
+fun main() {
+    println("Tuesday")
+    println("Thursday")
+    println("Wednesday")
+    println("Friday")
+    println("Monday")
+}
+
+Corrija o programa para que ele imprima esta saída:
+
+Monday
+Tuesday
+Wednesday
+Thursday
+Friday
+
+Resposta: */ fun main() {
+    	         println("Monday")
+                 println("Tuesday")
+                 println("Wednesday")
+    	         println("Thursday")
+    	         println("Friday")
+	     }
