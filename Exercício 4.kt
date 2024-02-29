c51f3796a858f0ad8f1e467e509002fd5400b8e1
Kauan Santos <125403442+KauanSantos789@users.noreply.github.com>
2024-02-28 22:22:23 -0300

    Add files via upload

---
+++ Exercício 4.kt
@@ -0,0 +1,12 @@
+/* Exercício 4. Corrija o erro neste programa, para que ele produza a saída desejada.
+
+fun main() {
+    println("Tomorrow is rainy")
+
+Saída desejada:
+
+Tomorrow is rainy
+
+Resposta: */ fun main() {
+    	         println("Tomorrow is rainy")
+	     }
