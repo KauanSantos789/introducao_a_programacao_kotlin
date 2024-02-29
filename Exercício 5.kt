2778a6f018f51f1f992722440e484d53a4456369
Kauan Santos <125403442+KauanSantos789@users.noreply.github.com>
2024-02-28 22:22:49 -0300

    Add files via upload

---
+++ Exercício 5.kt
@@ -0,0 +1,13 @@
+/* Exercício 5. Corrija o erro neste programa, para que ele produza a saída desejada.
+
+fun main() {
+    printLine("There is a chance of snow")
+}
+
+Saída desejada:
+
+There is a chance of snow
+
+Resposta: */ fun main() {
+                 println("There is a chance of snow")
+             }
