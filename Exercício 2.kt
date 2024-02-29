2b5923e6491efca90a5428e124710e3f57833da2
Kauan Santos <125403442+KauanSantos789@users.noreply.github.com>
2024-02-28 22:21:50 -0300

    Add files via upload

---
+++ Exercício 2.kt
@@ -0,0 +1,11 @@
+/* Exercício 2. Use o Kotlin Playground para criar um programa que produza as seguintes mensagens: 
+
+I'm
+learning
+Kotlin!
+
+Resposta: */ fun main() {
+                 println("I'm")
+                 println("learning")
+    	         println("Kotlin!")
+   	     }
